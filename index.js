c701b69bc9b92fc46628a5f3b4ab25e134fff6f3
wolfitis <webblog123@gmail.com>
2021-07-26 01:47:59 -0400

    fetch user public data

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ const redis = require('redis');
 const S_PORT = process.env.PORT || 3000;
 const R_PORT = process.env.PORT || 6379;
 
-const client = redis.client(R_PORT);
+const client = redis.createClient(R_PORT);
 
 const app = express();
 
+// Helper functions
+const getReposCount = async (req, res) => {
+  try {
+    const { username } = req.params;
+    // fetching data from github API
+    console.log(`Fetching data for ${username}`);
+    const respone = await fetch(`https://api.github.com/users/${username}`);
 
+    const data = await respone.json();
+
+    res.send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
 
 // Routes
 app.get('/repos/:username', getReposCount);
